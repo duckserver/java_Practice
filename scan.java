c960b397caf2f763100d07b3946d01256ff2f292
duckserver <79085028+duckserver@users.noreply.github.com>
2021-07-20 17:44:32 +0900

    Add files via upload

---
+++ scan.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class scan {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String input = scanner.next();
+        String input2 = scanner.next();
+        String input3 = scanner.next();
+        String input4 = scanner.next();
+
+        System.out.println(input);
+        System.out.println(input2);
+        System.out.println(input3);
+        System.out.println(input4);
+    }
+}
