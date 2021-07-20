6e5450b9dbe714ccbb2db7175a9a12b40eb5cd29
duckserver <79085028+duckserver@users.noreply.github.com>
2021-07-20 17:55:20 +0900

    Delete replaceall.java

--- replaceall.java
+++
@@ -1,7 +0,0 @@
-public class replaceall {
-    public static void main(String[] args) {
-        String birthday = "2000/10/21";
-        String real = birthday.replaceAll("2000/10", "1999/02");
-        System.out.println(real);
-    }
-}
